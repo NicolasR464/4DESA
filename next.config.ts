b4dec97a927bdb9a042345f0d8900886c9aca3dd
niiko <nicolas.rocagel@gmail.com>
2025-03-24 11:28:45 +0100

    Testing pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-    reactStrictMode: true,
-    distDir: 'build',
-    output: 'standalone',
-    env: {
-        MONGODB_URI: process.env.MONGODB_URI,
-        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
